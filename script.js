570f8a855412b63d94f1c05a07e13a2f3a78b9ce
ivan <kjs200095@gmail.com>
2020-12-26 14:50:09 +0300

    lesson_1

---
+++ script.js
@@ -0,0 +1,20 @@
+function Fahrenheit() {
+    let Tc = 24.0
+    let Tf = 0
+    Tf = 9 / 5 * Tc + 32
+    alert('Температура в Фарингейтах ' + Tf)
+}
+
+function giveName() {
+    let name = 'Василий'
+    let admin = name
+    alert(admin)
+}
+
+function Сonversion() {
+    let x = 1000
+    let y = '108'
+    let newY = Number(y)
+    let sum = x + newY
+    alert(sum)
+}
